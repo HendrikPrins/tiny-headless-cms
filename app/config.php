d6e3b967c68d04b1be138122e9bc851a085bcfc0
HendrikPrins <hawprins@gmail.com>
2025-11-25 16:08:00 +0100

    Update default locales

--- app/config.php
+++ app/config.php
@@ -48,7 +48,7 @@ if (!defined('CMS_DB_PASS')) define('CMS_DB_PASS', '');
 if (!defined('CMS_DB_NAME')) define('CMS_DB_NAME', 'cms_database');
 if (!defined('CMS_UPLOAD_DIR')) define('CMS_UPLOAD_DIR', __DIR__ . '/../uploads');
 
-const CMS_LOCALES = ['en', 'nl'];
+const CMS_LOCALES = ['nl', 'en'];
 const CMS_MAX_UPLOAD_SIZE = 1024 * 1020; // Default 1MB with a small buffer
 const CMS_ASSETS_TMP_DIR = 'temp';
 
